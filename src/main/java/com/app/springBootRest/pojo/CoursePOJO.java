f859c70d9910802f4284d0aa34a19de4cc9df806
Omkar Nikam <omkar.nikam24@gmail.com>
2017-11-09 18:07:33 +0530

    Misc

--- src/main/java/com/app/springBootRest/pojo/CoursePOJO.java
+++ src/main/java/com/app/springBootRest/pojo/CoursePOJO.java
@@ -1,7 +1,6 @@
 package com.app.springBootRest.pojo;
 
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/main/java/com/app/springBootRest/service/StudentService.java
+++ src/main/java/com/app/springBootRest/service/StudentService.java
@@ -1,11 +1,8 @@
 package com.app.springBootRest.service;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import com.app.springBootRest.pojo.CoursePOJO;
 import com.app.springBootRest.pojo.StudentPOJO;
 
 public interface StudentService extends CrudRepository<StudentPOJO, Integer>{
